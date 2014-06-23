32878347ed3a22332f3ec1021e731d387aea5f8c
Gordon Anderson <gordon.b.anderson@gmail.com>
2014-06-23 18:19:14 +0700

    ENHANCEMENT: Addition of map, updates for tweaked bulk uploader

--- code/GalleryPage.php
+++ code/GalleryPage.php
@@ -10,20 +10,45 @@ class GalleryPage extends Page {
 
   
    public function getCMSFields() {
-	   
    		$fields = parent::getCMSFields();
-		
-			$gridFieldConfig = GridFieldConfig_RecordEditor::create(); 
-			//$gridFieldConfig->addComponent(new GridFieldBulkEditingTools());
-			$gridFieldConfig->addComponent(new GridFieldBulkImageUpload());   
-			$gridFieldConfig->addComponent(new GridFieldSortableRows('SortOrder'));    
-			$galleryimagesi18n = _t('GalleryImage.PLURALNAME', "Gallery Images");
-			$gridfield = new GridField("GalleryImages", $galleryimagesi18n, $this->GalleryImages()->sort("SortOrder"), $gridFieldConfig);
-				
-			$fields->addFieldToTab('Root.'.$galleryimagesi18n, $gridfield);
-		
+		$gridFieldConfig = GridFieldConfig_RecordEditor::create(); 
+		//$gridFieldConfig->addComponent(new GridFieldBulkEditingTools());
+		$gridFieldConfig->addComponent($gbu = new GridFieldBulkUpload());  
+		$gridFieldConfig->addComponent($gbm = new GridFieldBulkManager()); 
+		$gridFieldConfig->addComponent(new GridFieldSortableRows('SortOrder'));    
+		$galleryimagesi18n = _t('GalleryImage.PLURALNAME', "Gallery Images");
+		//$gbu->setConfig('fileRelationName','Image');
+		$gbu->setConfig('folderName', 'galleries/'.$this->ID);
+		//$gbm->setConfig('editableFields', array('Title'));
+		$gbm->setConfig('fieldsNameBlacklist', array('Lat','Lon','ZoomLevel'));
+		$gridfield = new GridField("GalleryImages", $galleryimagesi18n, $this->GalleryImages()->sort("SortOrder"), $gridFieldConfig);
+		$fields->addFieldToTab('Root.'.$galleryimagesi18n, $gridfield);
 		return $fields;
-		
+  }
+
+
+
+  public function Map() {
+  	$photosWithLocation = $this->GalleryImages()->where('Lat != 0 AND Lon !=0');
+    if ($photosWithLocation->count() == 0) {
+      return 'No locations found'; // don't render a map
+    }
+    $map = $photosWithLocation->getRenderableMap();
+    // $map->setDelayLoadMapFunction( true );
+    $map->setZoom( 10 );
+    $map->setAdditionalCSSClasses( 'fullWidthMap' );
+    $map->setShowInlineMapDivStyle( true );
+    $map->setClusterer(true);
+    /*
+    foreach($this->MapLayers() as $layer) {
+      $map->addKML($layer->KmlFile()->getAbsoluteURL());
+    }
+    */
+
+
+    //$map->addKML('http://assets.tripodtravel.co.nz/cycling/meuang-nont-to-bang-sue-loop.kml');
+    $map->setSize('100%','600px');
+    return $map;
   }
 
 }
