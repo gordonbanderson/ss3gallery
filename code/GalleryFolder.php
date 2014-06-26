b8fc7a23e934066698d9a49a8ed9fc948596afc4
Gordon Anderson <gordon.b.anderson@gmail.com>
2014-06-26 20:19:48 +0700

    ENHANCEMENT: Container for gallery images

---
+++ code/GalleryFolder.php
@@ -0,0 +1,24 @@
+<?php 
+
+class GalleryFolder extends PageWithImage {
+
+	private static $allowed_children = array(
+		'GalleryFolder',
+		'GalleryPage'
+	);
+
+	public function getCMSFields() {
+		$fields = parent::getCMSFields();
+		$fields->renameField('Content','Brief Summary');
+		return $fields;
+	}
+
+	/* Summary is a remapping of the content */
+	public function Summary() {
+		return $this->Content;
+	}
+}
+
+class GalleryFolder_Controller extends PageWithImage_Controller {
+
+}
