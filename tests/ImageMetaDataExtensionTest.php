52e017d043cef10391977e0210c98165f54f6626
Gordon Anderson <gordon.b.anderson@gmail.com>
2016-02-06 00:16:48 +0700

    ENHANCEMENT: Test for exif data import

--- tests/ImageMetaDataExtensionTest.php
+++ tests/ImageMetaDataExtensionTest.php
@@ -4,7 +4,37 @@ class ImageMetaDataExtensionTest extends SapphireTest
 {
     public function testProcessExifData()
     {
-        $this->markTestSkipped('TODO');
+        $gi = new GalleryImage();
+
+        $gi->Title = 'Gallery Image Example';
+
+        $folder = Folder::find_or_make('/ImageMetaDataExtensionTest/');
+        $testfilePath = 'assets/ImageMetaDataExtensionTest/test.jpg'; // Important: No leading slash
+
+        $sourcePath = getcwd() . '/ss3gallery/tests/test.jpg';
+        copy($sourcePath, $testfilePath);
+        $image = new Image();
+        $image->Filename = $testfilePath;
+        // TODO This should be auto-detected
+        $image->ParentID = $folder->ID;
+        $image->write();
+
+        error_log('IMAGE FILENAME: ' . $image->ID);
+        $this->assertEquals('assets/ImageMetaDataExtensionTest/test.jpg', $image->Filename);
+
+        $gi->ImageID = $image->ID;
+
+        //This will trigger processExifData method
+        $gi->write();
+
+        $this->assertTrue($gi->ExifRead);
+        $this->assertEquals('2.4', $gi->Aperture);
+        $this->assertEquals('10/160', $gi->ShutterSpeed);
+        $this->assertEquals(400, $gi->ISO);
+        $this->assertEquals('2015:09:19 17:40:54', $gi->TakenAt);
+        $this->assertEquals(Image::ORIENTATION_PORTRAIT, $gi->Orientation);
+        $this->assertEquals(13.860741666667, $gi->Lat);
+        $this->assertEquals(100.44168916667, $gi->Lon);
     }
 
     public function testOnAfterWriteNOT()
