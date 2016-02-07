746c59d1f38ed6dc190be205a31566b1dd79ee34
Gordon Anderson <gordon.b.anderson@gmail.com>
2016-02-07 20:48:49 +0700

    WIP: Debugging why AttachedGalleryExtensionTest is breaking all the tests.  Culling method by method

--- tests/AttachedGalleryExtensionTest.php
+++ tests/AttachedGalleryExtensionTest.php
@@ -22,35 +22,4 @@ class AttachedGalleryExtensionTest extends TestWithImage {
         $this->assertEquals($expected, $names);
     }
 
-    public function testInlineGalleries() {
-        // this page is alone at the top level
-        $page = $this->objFromFixture('Page', 'page02');
-        $this->assertEquals(0, $page->InlineGalleries()->count());
-
-        // Check for an attached gallery
-        $gallery1 = $this->objFromFixture('GalleryPage', 'gp01');
-        $page->AttachedGalleryID = $gallery1->ID;
-        $page->write();
-        $expected = array($page);
-        $this->assertEquals('Test Gallery 1', $page->InlineGalleries()->first()->Title);
-        $this->assertEquals(1, $page->InlineGalleries()->count());
-
-        // Now add child galleries as well
-        $gallery2 = $this->objFromFixture('GalleryPage', 'gp02');
-        $gallery2->ParentID = $page->ID;
-        $gallery2->write();
-
-        $gallery3 = $this->objFromFixture('GalleryPage', 'gp03');
-        $gallery3->ParentID = $page->ID;
-        $gallery3->write();
-
-        $this->assertEquals(3, $page->InlineGalleries()->count());
-        $titles = array();
-        foreach ($page->InlineGalleries() as $gallery) {
-            array_push($titles, $gallery->Title);
-        }
-        $expected = array('Test Gallery 2', 'Test Gallery 3', 'Test Gallery 1');
-        $this->assertEquals($expected, $titles);
-
-    }
 }
