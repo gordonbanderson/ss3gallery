274a28d7907103b53e22b2ff31cb2cdcd947586a
Gordon Anderson <gordon.b.anderson@gmail.com>
2016-02-07 21:02:15 +0700

    WIP: Not many lines remaining now!

--- tests/AttachedGalleryExtensionTest.php
+++ tests/AttachedGalleryExtensionTest.php
@@ -3,7 +3,7 @@
 class AttachedGalleryExtensionTest extends TestWithImage {
     protected static $fixture_file = 'ss3gallery/tests/ss3gallery.yml';
 
-    protected $requiredExtensions = array('Page' => array('AttachedGalleryExtension'));
+    //protected $requiredExtensions = array('Page' => array('AttachedGalleryExtension'));
 
 
     /*
