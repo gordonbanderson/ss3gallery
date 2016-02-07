142cf77082e09027db6bef796fe7238d23e4df8b
Gordon Anderson <gordon.b.anderson@gmail.com>
2016-02-07 13:07:59 +0700

    ENHANCEMENT: Image tests should extend this

---
+++ tests/TestWithImage.php
@@ -0,0 +1,25 @@
+<?php
+
+class TestWithImage extends SapphireTest {
+    public function setUp() {
+        parent::setUp();
+
+        $folder = Folder::find_or_make('/ImageMetaDataExtensionTest/');
+        $testfilePath = 'assets/ImageMetaDataExtensionTest/test.jpg'; // Important: No leading slash
+
+        $sourcePath = getcwd() . '/ss3gallery/tests/test.jpg';
+        copy($sourcePath, $testfilePath);
+
+        $image = new Image();
+        $image->Title = 'Test Image';
+        $image->Filename = $testfilePath;
+        // TODO This should be auto-detected
+        $image->ParentID = $folder->ID;
+        $image->write();
+    }
+
+    public function tearDown() {
+        unlink('assets/ImageMetaDataExtensionTest/test.jpg');
+        parent::tearDown();
+    }
+}
