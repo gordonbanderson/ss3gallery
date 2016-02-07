26da840fab847d659f46e6bb2e5ff9a5213dcd75
Gordon Anderson <gordon.b.anderson@gmail.com>
2016-02-07 20:53:17 +0700

    WIP: Leaving only a shell of a test now

--- tests/AttachedGalleryExtensionTest.php
+++ tests/AttachedGalleryExtensionTest.php
@@ -5,21 +5,4 @@ class AttachedGalleryExtensionTest extends TestWithImage {
 
     protected $requiredExtensions = array('Page' => array('AttachedGalleryExtension'));
 
-
-    /*
-    Check for the attached gallery tab, and that a gallery can be attached
-     */
-    public function testGetCMSFields()
-    {
-        $page = new Page();
-        $tab = $page->getCMSFields()->fieldByName('Root.AttachedGallery');
-        $fields = $tab->FieldList();
-        $names = array();
-        foreach ($fields as $field) {
-            array_push($names, $field->getName());
-        }
-        $expected = array('AttachedGalleryID');
-        $this->assertEquals($expected, $names);
-    }
-
 }
