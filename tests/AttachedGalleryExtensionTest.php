4714ce3f91d3e1b88abf14cdf171ff320ec7fd50
Gordon Anderson <gordon.b.anderson@gmail.com>
2016-02-07 20:39:06 +0700

    Revert "Revert "Revert "WIP: Either this or gallery test that is causing problems"""
    This reverts commit 9b5a12fd159e2b3c703456b42a8e618f0fcd9291.

---
+++ tests/AttachedGalleryExtensionTest.php
@@ -0,0 +1,56 @@
+<?php
+
+class AttachedGalleryExtensionTest extends TestWithImage {
+    protected static $fixture_file = 'ss3gallery/tests/ss3gallery.yml';
+
+    protected $requiredExtensions = array('Page' => array('AttachedGalleryExtension'));
+
+
+    /*
+    Check for the attached gallery tab, and that a gallery can be attached
+     */
+    public function testGetCMSFields()
+    {
+        $page = new Page();
+        $tab = $page->getCMSFields()->fieldByName('Root.AttachedGallery');
+        $fields = $tab->FieldList();
+        $names = array();
+        foreach ($fields as $field) {
+            array_push($names, $field->getName());
+        }
+        $expected = array('AttachedGalleryID');
+        $this->assertEquals($expected, $names);
+    }
+
+    public function testInlineGalleries() {
+        // this page is alone at the top level
+        $page = $this->objFromFixture('Page', 'page02');
+        $this->assertEquals(0, $page->InlineGalleries()->count());
+
+        // Check for an attached gallery
+        $gallery1 = $this->objFromFixture('GalleryPage', 'gp01');
+        $page->AttachedGalleryID = $gallery1->ID;
+        $page->write();
+        $expected = array($page);
+        $this->assertEquals('Test Gallery 1', $page->InlineGalleries()->first()->Title);
+        $this->assertEquals(1, $page->InlineGalleries()->count());
+
+        // Now add child galleries as well
+        $gallery2 = $this->objFromFixture('GalleryPage', 'gp02');
+        $gallery2->ParentID = $page->ID;
+        $gallery2->write();
+
+        $gallery3 = $this->objFromFixture('GalleryPage', 'gp03');
+        $gallery3->ParentID = $page->ID;
+        $gallery3->write();
+
+        $this->assertEquals(3, $page->InlineGalleries()->count());
+        $titles = array();
+        foreach ($page->InlineGalleries() as $gallery) {
+            array_push($titles, $gallery->Title);
+        }
+        $expected = array('Test Gallery 2', 'Test Gallery 3', 'Test Gallery 1');
+        $this->assertEquals($expected, $titles);
+
+    }
+}
