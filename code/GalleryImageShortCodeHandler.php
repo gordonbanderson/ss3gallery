1e2d4469f1870d070192e1cb8a43d140bda89f93
Gordon Anderson <gordon.b.anderson@gmail.com>
2016-02-07 15:09:13 +0700

    MINOR: Comment tidy up

--- code/GalleryImageShortCodeHandler.php
+++ code/GalleryImageShortCodeHandler.php
@@ -2,12 +2,10 @@
 
 class GalleryImageShortCodeHandler
 {
-    // taken from http://www.ssbits.com/tutorials/2010/2-4-using-short-codes-to-embed-a-youtube-video/ and adapted for SS3
     public static function parse_gallery_image($arguments, $caption = null, $parser = null)
     {
-        //  public static function link_shortcode_handler($arguments, $content = null, $parser = null) {
 
-        // first things first, if we dont have a video ID, then we don't need to
+        // first things first, if we dont have an ID, then we don't need to
         // go any further
         if (empty($arguments['id'])) {
             return;
