579b35ccbce4e3b1df94a4929e886495f17c4d3b
Gordon Anderson <gordon.b.anderson@gmail.com>
2016-02-07 14:17:13 +0700

    WIP: Reverted debug

--- tests/TestWithImage.php
+++ tests/TestWithImage.php
@@ -4,27 +4,22 @@ class TestWithImage extends SapphireTest {
     public function setUp() {
         parent::setUp();
 
-        /*error_log('Test with image T1');
-
-        error_log('Test with image T2');
         $folder = Folder::find_or_make('/ImageMetaDataExtensionTest/');
         $testfilePath = 'assets/ImageMetaDataExtensionTest/test.jpg'; // Important: No leading slash
 
         $sourcePath = getcwd() . '/ss3gallery/tests/test.jpg';
         copy($sourcePath, $testfilePath);
-error_log('Test with image T3');
+
         $image = new Image();
         $image->Title = 'Test Image';
         $image->Filename = $testfilePath;
         // TODO This should be auto-detected
         $image->ParentID = $folder->ID;
         $image->write();
-        error_log('Test with image T4');
-        */
     }
 
     public function tearDown() {
-        //unlink('assets/ImageMetaDataExtensionTest/test.jpg');
+        unlink('assets/ImageMetaDataExtensionTest/test.jpg');
         parent::tearDown();
     }
 }
