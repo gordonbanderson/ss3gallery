dec46d66a0192689e60c101a8ce8aa1a508083db
Gordon Anderson <gordon.b.anderson@gmail.com>
2016-04-18 19:31:00 +0700

    ENHANCEMENT: Moved inline JS to a separate file due to ordering problems

---
+++ javascript/ss3gallery-pretty-photo.js
@@ -0,0 +1,9 @@
+(function($) {
+      $(document).ready(function() {
+        $(document).ready(function(){
+		    $("a[rel^='prettyPhoto']").prettyPhoto();
+		  });
+      });
+
+      alert('test');
+})(jQuery);
